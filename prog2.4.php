bfbe746ec15077df6108d5f98ce2c5b68a45343e
chitrapadia <chitra.padia127382@marwadiuniversity.ac.in>
2026-07-08 10:54:39 +0530

    Add files via upload

---
+++ prog2.4.php
@@ -0,0 +1,27 @@
+<?php
+    $name="chitra padia";
+
+    //1) strlen() // any string
+    echo strlen($name);
+    echo "<br></br>";
+
+    //2) strpos() //find some specific word,letter
+    echo strpos($name,"M",0);
+    echo "<br></br>";
+
+    //3) str_word_count()
+    echo str_word_count($name);
+    echo "<br></br>";
+
+    //4) strrev()
+    echo strrev($name);
+    echo "<br></br>";
+
+    //5) strtolower()
+    echo strtolower($name);
+    echo "<br></br>";
+
+    //6) strtoupper()
+    echo strtoupper($name);
+
+?>
