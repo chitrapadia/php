b2149d63b0a7a8ae91f2558900104444ae287bb8
chitrapadia <chitra.padia127382@marwadiuniversity.ac.in>
2026-07-08 10:55:23 +0530

    Add files via upload

---
+++ prog2.7.php
@@ -0,0 +1,14 @@
+<?php
+        date_default_timezone_set("Asia/Kolkata");
+    //1) HOUR()
+        echo date("h");
+        echo "<br></br>";
+    //2) MINUTE()
+        echo date("i");
+        echo "<br></br>";
+    //3) SECOND()
+        echo date("s");
+        echo "<br></br>";
+    //4) DATE_FORMAT().
+        echo date("h:i:s");
+?>
